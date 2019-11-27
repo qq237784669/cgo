c46aa1e241716397a6dad9ae4a650c57ef692a3e
asd <asd@1.com>
2019-11-27 16:49:33 +0800

    添加 车辆列表查询返回字段

--- cgo_db/src/main/java/com/cgo/db/service/web_module/login/UserService.java
+++ cgo_db/src/main/java/com/cgo/db/service/web_module/login/UserService.java
@@ -254,8 +254,8 @@ public class UserService implements IUserService {
             List<Map<String, Object>> vehicleListConvert=new ArrayList<>();
             for (Map<String, Object> map : vehicleList) {
 
-                String vorgId = map.get("OrgId").toString();
-                if (orgId.equals(vorgId)){
+                String vehicleOrgId = map.get("OrgId").toString();
+                if (orgId.equals(vehicleOrgId)){
                     Map<String,Object> vehicleNode=new HashMap<>();
                     vehicleNode.put("id",map.get("VehicleId"));
                     vehicleNode.put("name",map.get("PlateNum"));
