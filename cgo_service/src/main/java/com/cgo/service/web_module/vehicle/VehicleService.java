5e2cb314da093381d456d7cf9eb22e36564fa8be
asd <asd@1.com>
2019-12-05 15:38:03 +0800

    车辆列表 逻辑更新

--- cgo_service/src/main/java/com/cgo/service/web_module/vehicle/VehicleService.java
+++ cgo_service/src/main/java/com/cgo/service/web_module/vehicle/VehicleService.java
@@ -12,10 +12,7 @@ import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
@@ -60,7 +57,8 @@ public class VehicleService implements IVehicleService {
             List<Map<String, Object>> nodeAll = new ArrayList<>();
             // 分段查找出所有 一级节点的 所有子节点
             for (Map<String, Object> map : levelOneNode) {
-                this.findChildNode(map, organizationList, userId, vehicleList);
+
+                this.findChildNode(map, organizationList, userId, vehicleList,map,new ArrayList<>());
 
                 // 当前map 为一级切点 递归完成后 查询出所有 子节点的map
                 nodeAll.add(map);
@@ -128,14 +126,23 @@ public class VehicleService implements IVehicleService {
     }
 
     /**
-     * @param node             一级节点
-     * @param organizationList
-     * @param userId
-     * @param vehicleList      车辆列表
+     *
+     * @param node  当前node
+     * @param organizationList 路列表
+     * @param userId  用户id
+     * @param vehicleList  车辆列表
+     * @param preNode 上个节点
+     * @param deleteNode 需要删除的节点
      */
-    private void findChildNode(Map<String, Object> node, List<Map<String, Object>> organizationList, String userId, List<Map<String, Object>> vehicleList) {
+    private void findChildNode(Map<String, Object> node,
+                               List<Map<String, Object>> organizationList,
+                               String userId,
+                               List<Map<String, Object>> vehicleList,
+                               Map<String, Object> preNode,
+                               List<Map<String, Object>> deleteNode) {
+
         node.remove("SearchCode");
-        node.remove("OrgType");
+        String orgType = (String) node.remove("OrgType");
 
         String orgName = node.remove("OrgName").toString();
         String orgId = node.remove("OrgId").toString();
@@ -163,50 +170,63 @@ public class VehicleService implements IVehicleService {
         //如果不存在  代表当前节点是叶子节点那么查询 出车牌号信息然后retrun
         else {
             //根据节点OrgId  查询 对应的 车辆信息的车牌号
-
-            List<Map<String, Object>> vehicleListConvert = new ArrayList<>();
-
-            for (Map<String, Object> map : vehicleList) {
-
-                String vehicleOrgId = map.get("OrgId").toString();
-                if (orgId.equals(vehicleOrgId)) {
-                    Map<String, Object> vehicleNode = new HashMap<>();
-                    vehicleNode.put("id", map.get("VehicleId"));
-                    vehicleNode.put("name", map.get("PlateNum"));
-                    vehicleNode.put("orgId", map.get("OrgId"));
-                    vehicleNode.put("vehicleType", map.get("VehicleTypeName"));
-                    vehicleNode.put("simNum", map.get("SimNum"));
-                    vehicleNode.put("isOnline", map.get("IsOnline"));
-                    vehicleNode.put("hasCamera", "1");
-                    vehicleNode.put("dvrDeviceId", map.get("DVRDeviceId"));
-                    vehicleNode.put("searchCode", map.get("SearchCode"));
-                    vehicleNode.put("terminalType", map.get("TerminalTypeName"));
-
-                    if ("999".equals(map.get("DVRTypeCode").toString()) && "1".equals(globalConfig.getHasVideo())) {
-                        vehicleNode.put("dvrChannelNum", map.get("DVRChannelNum"));
-                    } else {
-                        vehicleNode.put("dvrChannelNum", "0");
-                    }
-
-                    vehicleNode.put("chanelNumInfo", null); //未翻译
-                    if (map.get("VehicleOfflineTimeOut") != null) {
-                        vehicleNode.put("vehicleOfflineTimeOut", map.get("VehicleOfflineTimeOut"));
-                    }
-
-                    vehicleNode.put("receTime", "");
-                    vehicleListConvert.add(vehicleNode);
-                }
-            }
-
+            List<Map<String, Object>> vehicleListConvert = getChildOrVehicleList(vehicleList, orgId);
             if (vehicleListConvert.size() > 0)
                 node.put("children", vehicleListConvert);
-
+            else {
+                deleteNode.add(node);
+            }
             return;
         }
 
-        // 遍历当前节点 得所有子节点
-        for (Map<String, Object> map : childNode) {
-            findChildNode(map, organizationList, userId, vehicleList);
-        }
+        //递归
+        childNode.forEach(item->findChildNode(item, organizationList, userId, vehicleList,node,deleteNode));
+
+        //删除 需要动态删除的 节点，
+        deleteNode.forEach(item ->childNode.remove(item));
+        deleteNode.clear();
+
+        //查询当前节点 下的直属 车辆 非 子节点
+        List<Map<String, Object>> vehicleListConvert = getChildOrVehicleList(vehicleList, orgId);
+        if (vehicleListConvert.size()>0)
+            childNode.addAll(vehicleListConvert);
     }
+
+    private List<Map<String, Object>> getChildOrVehicleList(List<Map<String, Object>> vehicleList, String orgId) {
+        List<Map<String, Object>> vehicleListConvert = new ArrayList<>();
+        for (Map<String, Object> map : vehicleList) {
+            String vehicleOrgId = map.get("OrgId").toString();
+            if (orgId.equals(vehicleOrgId)) {
+                // 添加ext 字段 更改 fieldName
+                Map<String, Object> vehicleNode = new HashMap<>();
+                vehicleNode.put("id", map.get("VehicleId"));
+                vehicleNode.put("name", map.get("PlateNum"));
+                vehicleNode.put("orgId", map.get("OrgId"));
+                vehicleNode.put("vehicleType", map.get("VehicleTypeName"));
+                vehicleNode.put("simNum", map.get("SimNum"));
+                vehicleNode.put("isOnline", map.get("IsOnline"));
+                vehicleNode.put("hasCamera", "1");
+                vehicleNode.put("dvrDeviceId", map.get("DVRDeviceId"));
+                vehicleNode.put("searchCode", map.get("SearchCode"));
+                vehicleNode.put("terminalType", map.get("TerminalTypeName"));
+
+                if ("999".equals(map.get("DVRTypeCode").toString()) && "1".equals(globalConfig.getHasVideo())) {
+                    vehicleNode.put("dvrChannelNum", map.get("DVRChannelNum"));
+                } else {
+                    vehicleNode.put("dvrChannelNum", "0");
+                }
+
+                vehicleNode.put("chanelNumInfo", null); //未翻译
+                if (map.get("VehicleOfflineTimeOut") != null) {
+                    vehicleNode.put("vehicleOfflineTimeOut", map.get("VehicleOfflineTimeOut"));
+                }
+
+                vehicleNode.put("receTime", "");
+                vehicleListConvert.add(vehicleNode);
+            }
+        }
+        return vehicleListConvert;
+    }
+
+
 }
