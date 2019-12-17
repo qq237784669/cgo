aa7c492a4d2e43f7e32494e315e9717dcd987cfc
asd <asd@1.com>
2019-12-17 14:25:22 +0800

    add cros Filter

--- cgo_web/src/main/java/com/cgo/mobile/config/cros/CorsFilter.java
+++ cgo_web/src/main/java/com/cgo/mobile/config/cros/CorsFilter.java
@@ -1,5 +1,7 @@
 package com.cgo.mobile.config.cros;
 
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
@@ -9,21 +11,29 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
-@WebFilter(filterName = "crosFilter",urlPatterns = {"/*"})
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        System.out.println("aaa");
-        HttpServletResponse resp = (HttpServletResponse) response;
-        HttpServletRequest req = (HttpServletRequest) request;
-        resp.setHeader("Access-Control-Allow-Origin", "*");
-        resp.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
-        resp.setHeader("Access-Control-Max-Age", "3600");
-        resp.setHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type, Accept, Origin");
-        resp.setHeader("Access-Control-Allow-Credentials", "true");
-        chain.doFilter(req, resp);
+    public void doFilter(ServletRequest req, ServletResponse rep, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) req;
+        HttpServletRequest request = (HttpServletRequest) rep;
+        //允许所有的域访问
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        //允许所有方式的请求
+        response.setHeader("Access-Control-Allow-Methods", "*");
+        //头信息缓存有效时长（如果不设 Chromium 同时规定了一个默认值 5 秒），没有缓存将已OPTIONS进行预请求
+        response.setHeader("Access-Control-Max-Age", "3600");
+        //允许的头信息
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization");
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, rep);
+        }
     }
+
 
 
 
