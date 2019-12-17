07d315f3238faf33beadfe9ae241f8060a1310aa
asd <asd@1.com>
2019-12-17 14:34:51 +0800

    add cros Filter

--- cgo_login/src/main/java/com/cgo/login/config/auth/WebSecurityConfig.java
+++ cgo_login/src/main/java/com/cgo/login/config/auth/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsUtils;
 
 
 @Configuration
@@ -35,6 +36,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 "/swagger-resources/configuration/security",
                 "/swagger-ui.html",
                 "/webjars/**").permitAll()
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
                 .anyRequest().authenticated()
                 // .anyRequest().permitAll()
                 .and().formLogin();
