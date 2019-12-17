8d2a8a154fc407f9d1bf8172316e73150b9276f7
asd <asd@1.com>
2019-12-17 14:49:34 +0800

    add cros Filter

--- cgo_login/src/main/java/com/cgo/login/config/auth/WebSecurityConfig.java
+++ cgo_login/src/main/java/com/cgo/login/config/auth/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.cgo.login.config.auth;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +13,7 @@ import org.springframework.web.cors.CorsUtils;
 
 @Configuration
 @EnableWebSecurity
+@Order(-1)
 //这个类主要是做一个  授权之前的认证  如果美誉这个的话 直接去授权拿  认证码 的话 会报错  必须在授权之前
 // 先认证   认证后 才能 拿授权码   -------也就是 在拿授权码 之前 做一个认证
 // 即是  授权服务器 也是资源服务器
